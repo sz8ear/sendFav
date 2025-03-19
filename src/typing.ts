9813189d5889c8a433a89805c02914be48820445
P0lar8ear <m.s35gyllt@gmail.com>
2025-03-19 16:52:46 +0900

    fix: change .js file to .ts file

---
+++ src/typing.ts
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const typingElement = document.getElementById(
+    "typing-text"
+  ) as HTMLElement | null;
+  const codeSection = document.querySelector(
+    ".code-section"
+  ) as HTMLElement | null;
+  const scriptContainer = document.querySelector(
+    ".script-section"
+  ) as HTMLElement | null;
+
+  if (!typingElement || !codeSection || !scriptContainer) return;
+
+  const text: string | null = typingElement.getAttribute("data-text");
+  if (!text) return; // `data-text` がない場合は処理を終了
+
+  let index = 0;
+
+  function type(): void {
+    if (text && index < text.length) {
+      if (typingElement) {
+        typingElement.innerHTML = text.substring(0, index + 1);
+      }
+      index++;
+      setTimeout(type, 100); // タイピング速度を調整
+    } else {
+      if (scriptContainer) {
+        scriptContainer.classList.add("show"); // script タグを表示
+      }
+      setTimeout(() => {
+        if (codeSection) {
+          codeSection.classList.add("show"); // code-section を表示
+        }
+      }, 1000);
+    }
+  }
+
+  typingElement.innerHTML = "";
+  type(); // タイピング開始
+});
